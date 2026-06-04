8f89e8b54c662025ba868f8b41765d855b81a1e3
Suraj Shukla <140895314+Suraj-shukla94@users.noreply.github.com>
2026-06-04 10:04:36 +0000

    DAY 4 : CONSTANT AND VARIABLE OF C PROGRAM

---
+++ PRACTICE/MONTH1/WEEK 1/D4/d4_variable.c
@@ -0,0 +1,26 @@
+// DAY 4 - C_08 
+/* TOPICS : VARIABLES IN C
+1. DECLARATION 
+2. INITIALIZATION 
+*/
+
+// DATED - 04/06/2026
+
+// USE OF VARIABLES IN C PROGRAM 
+
+// DECLARE :
+// DATATYPE VARIABLE_NAME
+int a; // declaration of variable 
+a=10; // initialization of variable
+int a=10; // declaration and initialization of variable 
+
+// float a;
+// char a;
+// int a=1 , b=2; // multiple declaration and initialization of variable 
+
+// RULES FOR CONSTRUCTING VARIABLE NAME 
+// 1. VARIABLE NAME MUST START WITH ALPHABET OR UNDERSCORE 
+// 2. VARIABLE NAME CAN CONTAIN ALPHABET , DIGIT OR UNDERSCORE 
+// 3. VARIABLE NAME CANNOT CONATIN SPACE 
+// 4. VARIABLE NAME CANNOT BE KEYWORD 
+// 5. VARIABLE NAME CANNOT BE SAME AS FUNCTION NAME 
