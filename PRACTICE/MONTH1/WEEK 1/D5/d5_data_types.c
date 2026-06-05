210079bf818fa1eb103400cb153bed4c014aa14d
Suraj Shukla <140895314+Suraj-shukla94@users.noreply.github.com>
2026-06-05 10:00:26 +0000

    DAY 5 : KEYWORDS AND IDENTIFIERS AND DATA TYPE OF C LANGUAGE

---
+++ PRACTICE/MONTH1/WEEK 1/D5/d5_data_types.c
@@ -0,0 +1,24 @@
+// DAY 5 - C_10
+// TOPIC - DATA TYPES IN C
+// DATE - 05/06/2026
+
+/* DATA TYPES : 
+   1. PRIMARY (BUILT IN)
+      - INT
+      - FLOAT
+      - CHAR
+      - DOUBLE
+      - VOID
+   2. DERIVED (USER DEFINED)
+      - ARRAY
+      - POINTER
+      - STRUCTURE
+      - UNION 
+   3. USER DEFINED 
+      - ENUMERATED 
+      - TYPEDEF 
+      **typedef int welcome;
+        welcome a; **
+*/
+
+// THEORY OF DATA TYPE
---
+++ PRACTICE/MONTH1/WEEK 1/D5/d5_keywords&identifier.c
@@ -0,0 +1,17 @@
+// DAY 5 - C_09
+/* TOPICS : 
+   1. KEYWORDS
+   2. IDENTIFIERS
+*/
+// DATED - 05/06/2026
+
+/* KEYWORDS : 
+   1. Keywords are reserved words that have special meanings to the compiler.
+
+if , for , int , float , while , do , "etc"
+*/
+
+/* IDENTIFIERS :
+   1. Identifiers are user-defined names given to variables, functions, arrays, etc.
+   2. They can contain digits, alphabets and underscores.
+*/
