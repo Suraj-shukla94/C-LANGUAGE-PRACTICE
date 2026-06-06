5626f994165307c6cab7759a75e21d5c5a838991
Suraj Shukla <140895314+Suraj-shukla94@users.noreply.github.com>
2026-06-06 09:46:07 +0000

    DAY 6 : DATA TYPE THEORY AND CONCEPT

---
+++ PRACTICE/MONTH1/WEEK 1/D6/d6_data_type_2.c
@@ -0,0 +1,18 @@
+// DAY 6 - C_11
+// TOPICS : DATA TYPE PART 2 
+// DATE - 06/06/2026
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 32768;
+    int b = -1;
+
+    printf("Overflow Example: %d\n", a);
+
+    printf("Signed: %d\n", b);
+    printf("Unsigned: %u\n", b);
+
+    return 0;
+}
---
+++ PRACTICE/MONTH1/WEEK 1/D6/d6_data_type_3.c
@@ -0,0 +1,24 @@
+// DAY 6 - C_12
+// TOPIC : DATA TYPE PART 3
+// DATE - 06/06/2026
+
+#include <stdio.h>
+
+int main()
+{
+    char ch = 'A';
+
+    printf("Character = %c\n", ch);
+    printf("ASCII Value = %d\n", ch);
+
+    char ch2 = 65;
+
+    printf("Character from ASCII = %c\n", ch2);
+
+    return 0;
+}
+
+/* OUTPUT : 
+Character = A
+ASCII Value = 65
+Character from ASCII = A */
